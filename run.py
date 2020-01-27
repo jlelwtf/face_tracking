7b66041222ddecd384566e9bfcdff95fedac153d
jlel <jlelwtf@gmail.com>
2020-01-27 10:43:42 +0300

    refactor run script

--- run.py
+++ run.py
@@ -1,12 +1,16 @@
+import argparse
+import sys
+
 import cv2
 import imutils
 
 from face_detector import FaceDetector
 from face_tracker import FaceTracker
 
-video_capture = cv2.VideoCapture('video/4p-c0.avi')
-# video_capture = cv2.VideoCapture(0)
-face_rect_detector = FaceDetector()
+
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument('--video_path', default='video/4p-c0.avi')
+arg_parser.add_argument('--scale_view', type=float, default=3.)
 
 
 def draw_labeled_rectangle(image, rectangle, text):
@@ -19,21 +23,23 @@ def draw_labeled_rectangle(image, rectangle, text):
 
 
 if __name__ == '__main__':
+    argv = arg_parser.parse_args(sys.argv[1:])
+
+    video_capture = cv2.VideoCapture(argv.video_path)
+    face_rect_detector = FaceDetector()
+
     ret = True
-    ratio = 4.
+    ratio = argv.scale_view
     tracker = FaceTracker()
     while ret:
         ret, frame = video_capture.read()
         if ret:
             rects = face_rect_detector(frame)
             faces = tracker.track(rects, frame)
-
             frame = imutils.resize(frame, width=int(frame.shape[1] * ratio))
-            # rects = [rect.scale(ratio) for rect in rects]
-
             for face in faces:
                 rect = face.rectangle
                 rect = rect.scale(ratio).expand(10)
                 draw_labeled_rectangle(frame, rect, str(face.id))
-            cv2.imshow('video', frame)
+            cv2.imshow('face tracking', frame)
             cv2.waitKey(1)
